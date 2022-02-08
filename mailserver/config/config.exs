dbbcce117b817e0ca3bb8564bdab09eff34aa725
matthiass2 <matias_69914081@hotmail.com>
2022-02-08 02:50:37 -0300

    fix(Mailtrap): remove smtp server credentials

--- mailserver/config/config.exs
+++ mailserver/config/config.exs
@@ -2,11 +2,11 @@ use Mix.Config
 
 config :mailserver, Mailserver.Mailer,
   adapter: Bamboo.SMTPAdapter,
-  server: "smtp.mailtrap.io",
-  hostname: "smtp.mailtrap.io",
+  server: "SERVER",
+  hostname: "SERVER",
   port: 2525,
-  username: "8f1bc8a8b09c78", # or {:system, "SMTP_USERNAME"}
-  password: "0ca8c38cab5f7c", # or {:system, "SMTP_PASSWORD"}
+  username: "USER", # or {:system, "SMTP_USERNAME"}
+  password: "PASS", # or {:system, "SMTP_PASSWORD"}
   tls: :always, # can be `:always` or `:never`
   allowed_tls_versions: [:"tlsv1", :"tlsv1.1", :"tlsv1.2"], # or {:system, "ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
   ssl: false, # can be `true`
